41e5f1b9f43e96a2fd1f303f64546c95e8e1e63f
arema-arega <128746733+arema-arega@users.noreply.github.com>
2023-12-28 22:53:42 +0100

    Deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/inter-art/',
+  base: '/inter-art/client/', // Update this to match your repository name and structure
+  assetsDir: 'assets', // Update this to match your actual assets directory
   plugins: [react()],
   server: {
       proxy: {
