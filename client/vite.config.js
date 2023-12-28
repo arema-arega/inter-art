3768b5ea6fd2a89fabae7d9f8d411e93ace0783c
arema-arega <128746733+arema-arega@users.noreply.github.com>
2023-12-29 00:12:18 +0100

    Trying to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/inter-art', // Update this to match your repository name and structure
+  base: '/inter-art/client/build/',
   plugins: [react()],
   server: {
       proxy: {
